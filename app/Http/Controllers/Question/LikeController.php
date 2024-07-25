3f5e207f47cda24086b3da01ee4263ea3de7775c
VitorNuness <vitor1nuness@gmail.com>
2024-07-25 12:27:47 -0300

    HEY-11: refactoring like for not be able to like more than 1 time

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question};
+use App\Models\{Question, User};
 use Illuminate\Http\RedirectResponse;
 
 class LikeController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
-        auth()->user()->like($question);
+        /** @var User $user */
+        $user = auth()->user();
+        $user->like($question);
 
         return back();
     }
