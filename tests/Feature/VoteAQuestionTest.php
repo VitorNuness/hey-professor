6100f1ce1fc3549e30927a90cddc0a93e9fed4a8
VitorNuness <vitor1nuness@gmail.com>
2024-07-25 11:51:04 -0300

    HEY-11: be able to like a question

---
+++ tests/Feature/VoteAQuestionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, assertDatabaseHas, post};
+
+it('should be able to like a question', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    actingAs($user);
+    $response = post(route('question.like', $question));
+
+    $response->assertRedirect();
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'like'        => 1,
+        'unlike'      => 0,
+        'user_id'     => $user->id,
+    ]);
+});
