c094973107d573c7c2f1d8d4d9d33589e49df0d5
VitorNuness <vitor1nuness@gmail.com>
2024-07-25 21:29:38 -0300

    HEY-15: it should make sure that only the person who has created the question can publish the question

--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -6,7 +6,9 @@ use function Pest\Laravel\{actingAs, put};
 
 it('should be able to publish a question', function () {
     $user     = User::factory()->create();
-    $question = Question::factory()->create(['is_draft' => true]);
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['is_draft' => true]);
 
     actingAs($user);
     put(route('question.publish', $question))
@@ -14,4 +16,18 @@ it('should be able to publish a question', function () {
     $question->refresh();
 
     expect($question->is_draft)->toBe(0);
+});
+
+it('should make sure that only the person who has created the question can publish the question', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['is_draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+    put(route('question.publish', $question))
+        ->assertForbidden();
+
+    actingAs($rightUser);
+    put(route('question.publish', $question))
+        ->assertRedirect();
 });
