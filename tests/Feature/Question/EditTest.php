1f9f38c9267cbeb0da54f5e0d02b29586c274c6e
VitorNuness <vitor1nuness@gmail.com>
2024-07-26 10:58:53 -0300

    HEY-17: is should make sure that only question with status DRAFT can be edited

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -14,3 +14,30 @@ it('should be able to open a question to edit', function () {
     get(route('question.edit', $question))
         ->assertSuccessful();
 });
+
+it('should return a view', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['is_draft' => true]);
+
+    actingAs($user);
+    get(route('question.edit', $question))
+        ->assertViewIs('question.edit');
+});
+
+it('should make sure that only question with status DRAFT can be edited', function () {
+    $user          = User::factory()->create();
+    $draftQuestion = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['is_draft' => true]);
+    $publishedQuestion = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['is_draft' => false]);
+
+    actingAs($user);
+    get(route('question.edit', $publishedQuestion))
+        ->assertForbidden();
+    get(route('question.edit', $draftQuestion))
+        ->assertSuccessful();
+});
