f5ccd058cf06f3a30160606c6c80a8ea429f9ce8
VitorNuness <vitor1nuness@gmail.com>
2024-07-26 11:04:54 -0300

    HEY-17: should make sure that only the person who has created the question can edit the question

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -13,7 +13,8 @@ class QuestionPolicy
 
     public function update(User $user, Question $question): bool
     {
-        return $question->is_draft;
+        return $question->is_draft
+            && $question->createdBy->is($user);
     }
 
     public function destroy(User $user, Question $question): bool
