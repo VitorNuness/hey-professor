e25a932413d889899290258b9aef685209ab27c8
VitorNuness <vitor1nuness@gmail.com>
2024-07-26 10:37:09 -0300

    HEY-17: it should be able to open a question to edit

---
+++ tests/Feature/Question/EditTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, get};
+
+it('should be able to open a question to edit', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['is_draft' => true]);
+
+    actingAs($user);
+    get(route('question.edit', $question))
+        ->assertSuccessful();
+});
