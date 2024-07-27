90035624ef5fc00ad23bbe77049ac9cda577ddc7
VitorNuness <vitor1nuness@gmail.com>
2024-07-27 00:10:09 -0300

    HEY-24: applying filter in the frontend

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,16 @@
 
     <x-container>
         <div class="dark:text-gray-400 space-y-4">
-            @foreach ($questions as $item)
+            <x-search :action="route('dashboard')" placeholder="Search for a question" />
+
+            @forelse ($questions as $item)
                 <x-question :question="$item" />
-            @endforeach
+            @empty
+                <div class="dark:text-gray-300 text-center flex flex-col justify-center items-center gap-4 p-8">
+                    <x-draws.not-found class="w-96" />
+                    <p>Ops! Don't find any question with have "<span class="font-bold">{{ request()->search }}</span>".</p>
+                </div>
+            @endforelse
 
             {{ $questions->withQueryString()->links() }}
         </div>
